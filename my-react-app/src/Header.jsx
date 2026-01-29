e4e27949ad632fc9138b94470101ee58e51896e6
Victor Usen <usenvictor40@gmail.com>
2026-01-29 11:51:43 +0100

    Update Header.jsx

--- my-react-app/src/Header.jsx
+++ my-react-app/src/Header.jsx
@@ -7,10 +7,11 @@ function Header() {
     <header className='max-md:px-10 flex absolute z-10 top-0 left-0 right-0 w-full px-20 h-30 gap-20 md:bg-transparent items-center'>
       <div className="max-md:w-full flex items-center justify-between ">
         <img className='md:hidden' src={ hamburger } />
-        <a className='max-md:hidden' href="/"><img src={ logo } alt="logo" /></a> 
+        <div className='flex max-xl:justify-center items-center w-full'>
+          <a className='' href="/"><img src={ logo } alt="logo" /></a> 
+        </div>
       </div>
-      <div className="flex justify-center items-center ">
-        <a className='block md:hidden' href="/"><img src={ logo } alt="logo" /></a> 
+      <div className="flex items-center ">
         <nav className='max-md:hidden'>
           <ul className='flex gap-6 text-white' >
             <li><a id='a' href="#home">home</a></li>
